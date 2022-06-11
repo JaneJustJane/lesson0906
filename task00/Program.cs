c607b473c452217817407bd3bcf998f9088db910
Eugenia Galkina <yavsehmilee@gmail.com>
2022-06-11 17:39:57 +0300

    recurcion amount of digits of number

---
+++ task00/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму цифр числа с помощью рекурсии
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()?? "0");
+int amount = 0;
+
+
+
+int Sum (int n)
+{
+    if (n%10 == 0) return n%10;
+    else  Sum (n/10);
+    amount = amount + n%10;
+    return amount;
+}
+
+Sum(n);
+Console.WriteLine (amount);
+
