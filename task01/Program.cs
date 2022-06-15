6cc58b98c79bd4ecda443e1bd801ef624075826d
Eugenia Galkina <yavsehmilee@gmail.com>
2022-06-15 16:10:11 +0300

    a^b by recurcion

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения целого числа А в целую степень В (с помощью рекурсии)
+
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Введите степень В, в которую возвести число А: ");
+int b = int.Parse(Console.ReadLine()?? "0");
+
+int Power(int a, int b)
+{
+    if (b==0) return 1;
+    else return Power(a,b-1)*a;
+}
+Console.WriteLine(Power(a,b));
