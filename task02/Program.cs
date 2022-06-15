7ba84d3085d574ca75e7ee9b3803a406110c523f
Eugenia Galkina <yavsehmilee@gmail.com>
2022-06-15 16:52:22 +0300

    amount of the next number is the sum of 2 previous

---
+++ task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Показать первые N чисел, для которых каждое следующее = суммме двух предыдущих
+// Первые 2 элемента последовательности задаются пользователем. 
+
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Введите число В: ");
+int b = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Введите количество чисел N: ");
+int n = int.Parse(Console.ReadLine()?? "0");
+Console.Write($"{a} ");
+
+string SUM(int a, int b, int n)
+{
+    Console.Write($"{b} ");
+    if (n==2) return String.Empty;
+    else return SUM(b, a+b, n-1);
+}
+SUM(a,b,n);
